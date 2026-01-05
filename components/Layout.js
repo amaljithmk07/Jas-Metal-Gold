841530409002308330f267de6487e1c14c7619e1
amaljithmk07 <amaljithmk44@gmail.com>
2026-01-05 12:38:07 +0530

    Purity Charges

--- components/Layout.js
+++ components/Layout.js
@@ -1,33 +1,30 @@
 import React, { useEffect, useState } from "react";
-
-
-
+import { SpotRateProvider } from "@/context/SpotRateContext";
 
 const Layout = (props) => {
-
-
-
-
-
     return (
         <React.Fragment>
-            <main
-                id="main-element"
-                className={`main`}
-            >
-
+            <main id="main-element" className={`main`}>
                 <head>
                     <title>Jas Metal Gold</title>
-                    <meta name="description" content="MindFlow is a trusted companion for women, helping you track your period cycles and care for your mental health. Like an intimate friend, always here for your well-being." />
+                    <meta
+                        name="description"
+                        content="MindFlow is a trusted companion for women, helping you track your period cycles and care for your mental health. Like an intimate friend, always here for your well-being."
+                    />
 
                     <link rel="icon" href="/favicon.ico" sizes="any" />
 
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0"
+                    />
                     <meta charSet="UTF-8" />
                 </head>
 
                 {/* < Header /> */}
-                {props.children}
+                {/* {props.children} */}
+
+                <SpotRateProvider>{props.children}</SpotRateProvider>
                 {/* <Footer /> */}
             </main>
         </React.Fragment>
--- components/MainScreen/MainScreen.js
+++ components/MainScreen/MainScreen.js
@@ -1,36 +1,14 @@
+'use client'
+
 import React, { useState, useEffect } from "react";
 import styles from "./MainScreen.module.scss";
 import Image from "next/image";
 import TradingViewMarketTable from "../TradingViewChart/TradingViewChart";
+import io from "socket.io-client";
+import { fetchSpotRates, fetchServerURL, fetchNews, fetchTVScreenData } from "@/api/api";
+import { useSpotRate } from "@/context/SpotRateContext";
 
 const MainScreen = () => {
-  const commodities = [
-    {
-      commodity: "Ten Tola Bar 1 TTB",
-      bid: 54671,
-      ask: 54671,
-    },
-    {
-      commodity: "Kilo Bar 9999 1 KG",
-      bid: 54671,
-      ask: 54671,
-    },
-    {
-      commodity: "Kilo Bar 995 1 KG",
-      bid: 54671,
-      ask: 54671,
-    },
-    {
-      commodity: "Gold 9999 1 GM",
-      bid: 54671,
-      ask: 54671,
-    },
-    {
-      commodity: "Gold 22 K",
-      bid: 54671,
-      ask: 54671,
-    },
-  ];
 
   const getFormattedDate = () => {
     const today = new Date();
@@ -52,6 +30,169 @@ const MainScreen = () => {
   const { date, day } = getFormattedDate();
   const marqueeText = 'CRUDE OIL : $65.70 (9.9%)';
 
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  const [serverURL, setServerURL] = useState("");
+  const [marketData, setMarketData] = useState({});
+  const [commodities, setCommodities] = useState([]);
+  const [news, setNews] = useState([]);
+
+  const [goldBidSpread, setGoldBidSpread] = useState(0);
+  const [goldAskSpread, setGoldAskSpread] = useState(0);
+  const [silverBidSpread, setSilverBidSpread] = useState(0);
+  const [silverAskSpread, setSilverAskSpread] = useState(0);
+
+
+
+  const adminId = process.env.NEXT_PUBLIC_ADMIN_ID;
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      const res = await fetchSpotRates(adminId);
+      const info = res.data.info;
+
+      setCommodities(info.commodities || []);
+
+      setGoldBidSpread(info.goldBidSpread || 0);
+      setGoldAskSpread(info.goldAskSpread || 0);
+      setSilverBidSpread(info.silverBidSpread || 0);
+      setSilverAskSpread(info.silverAskSpread || 0);
+    };
+
+    fetchInitialData();
+  }, [adminId]);
+
+  const { updateMarketData } = useSpotRate();
+
+  useEffect(() => {
+    if (!marketData || Object.keys(marketData).length === 0) return;
+
+    updateMarketData(
+      marketData,
+      goldBidSpread,
+      goldAskSpread,
+      silverBidSpread,
+      silverAskSpread
+    );
+  }, [
+    marketData,
+    goldBidSpread,
+    goldAskSpread,
+    silverBidSpread,
+    silverAskSpread,
+  ]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      try {
+        const [spotRatesRes, serverURLRes, newsRes] = await Promise.all([
+          fetchSpotRates(adminId),
+          fetchServerURL(),
+          fetchNews(adminId),
+        ]);
+
+        setCommodities(spotRatesRes.data.info.commodities);
+        setServerURL(serverURLRes.data.info.serverURL);
+        // setNews(newsRes.data.news.news);
+      } catch (error) {
+        console.error("Failed to load initial data", error);
+      }
+    };
+
+    fetchInitialData();
+  }, [adminId]);
+
+  useEffect(() => {
+    if (!serverURL) return;
+
+    const socket = io(serverURL, {
+      query: {
+        secret: process.env.NEXT_PUBLIC_SOCKET_SECRET_KEY,
+      },
+      transports: ["websocket"],
+      withCredentials: true,
+    });
+
+    socket.on("connect", () => {
+      socket.emit("request-data", ["GOLD", "SILVER"]);
+    });
+
+    socket.on("market-data", (data) => {
+      setMarketData((prev) => ({
+        ...prev,
+        [data.symbol]: data,
+      }));
+    });
+
+    socket.on("disconnect", () => {
+      // console.log("Socket disconnected");
+
+    });
+
+    return () => socket.disconnect();
+  }, [serverURL]);
+  const { goldData, silverData } = useSpotRate();
+
+
+  /* ---------------- HELPERS ---------------- */
+
+  const getBidAskValues = (metal) => {
+    if (
+      metal === "gold" ||
+      metal === "gold kilobar" ||
+      metal === "gold ten tola"
+    ) {
+      return {
+        bid: parseFloat(goldData?.bid) || 0,
+        ask: parseFloat(goldData?.ask) || 0,
+      };
+    }
+
+    if (metal === "silver") {
+      return {
+        bid: parseFloat(silverData?.bid) || 0,
+        ask: parseFloat(silverData?.ask) || 0,
+      };
+    }
+
+    return { bid: 0, ask: 0 };
+  };
+
+  /* ---------------- API ---------------- */
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      try {
+        const res = await fetchSpotRates(adminId);
+        setCommodities(res.data.info.commodities || []);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchInitialData();
+  }, [adminId]);
+
+
+  const getMetalName = (metal) => {
+    switch (metal.toLowerCase()) {
+      case "gold":
+        return "GM";
+      case "gold kilobar":
+        return "KGBAR";
+      case "gold ten tola":
+        return "TTBAR";
+      default:
+        return metal.charAt(0).toUpperCase() + metal.slice(1);
+    }
+  };
 
   return (
     <div className={styles.mainscreen_Section}>
@@ -68,16 +209,44 @@ const MainScreen = () => {
                   <h3>ASK (Dollar)</h3>
                 </div>
               </li>
+              {commodities.map((item, index) => {
+                const metalName = getMetalName(item.metal.toLowerCase());
+                const bid = marketData?.Gold?.bid;
+                const ask = marketData?.Gold?.offer;
+                const unitMultiplier = {
+                  GM: 1,
+                  KGBAR: 1000,
+                  TTBAR: 116.64,
+                }[metalName];
 
-              {commodities.map((data) => (
-                <li className={`${styles.detail_content_sec}`}>
-                  <div className={`${styles.detail_sec}`}>
-                    <span>{data.commodity}</span>
-                    <span>{data.bid}</span>
-                    <span>{data.ask}</span>
-                  </div>
-                </li>
-              ))}
+                const purity = Number(item.purity);
+
+
+                const purityPower = purity / Math.pow(10, purity.toString().length);
+
+                const bidPrice =
+                  ((bid / 31.103) * 3.674) *
+                  unitMultiplier * purityPower;
+
+                const askPrice =
+                  ((ask / 31.103) * 3.674) *
+                  unitMultiplier * purityPower;
+
+                return (
+
+                  <li
+                    key={index}
+                    className={styles.detail_content_sec}
+                  >
+                    <div className={styles.detail_sec}>
+                      <span>{item.purity} {metalName}</span>
+                      <span>{bidPrice.toFixed(2)}</span>
+                      <span>{askPrice.toFixed(2)}</span>
+                    </div>
+                  </li>
+                );
+              })}
+
             </ul>
 
             <div className={`${styles.price_section}`}>
@@ -91,12 +260,13 @@ const MainScreen = () => {
                       alt=""
                     />
                   </div>
+
                   <div className={`${styles.price_text}`}>
                     <span className={styles.label}>High </span>
-                    <span className={styles.value}> 1,234.00</span>
+                    <span className={styles.value}> {marketData?.Gold?.high}</span>
                     <span className={styles.separator}>/</span>
                     <span className={styles.label}>Low </span>
-                    <span className={styles.value}> 1,234.00</span>
+                    <span className={styles.value}>{marketData?.Gold?.low}</span>
                   </div>
                   <div className={`${styles.bar_icon}`}>
                     <Image
@@ -110,14 +280,14 @@ const MainScreen = () => {
                   <div className={styles.bidAsk_wrap}>
                     <div className={styles.bid}>
                       <span className={styles.label}>BID</span>
-                      <span className={styles.bidValue}>1,234.00</span>
+                      <span className={styles.bidValue}>{marketData?.Gold?.bid}</span>
                     </div>
                   </div>
 
                   <div className={styles.bidAsk_wrap}>
                     <div className={styles.ask}>
                       <span className={styles.label}>ASK</span>
-                      <span className={styles.askValue}>1,234.00</span>
+                      <span className={styles.askValue}>{marketData?.Gold?.offer}</span>
                     </div>
                   </div>
                 </li>
@@ -132,11 +302,12 @@ const MainScreen = () => {
                     />
                   </div>
                   <div className={`${styles.price_text}`}>
+
                     <span className={styles.label}>High </span>
-                    <span className={styles.value}> 1,234.00</span>
+                    <span className={styles.value}>{marketData?.Silver?.high}</span>
                     <span className={styles.separator}>/</span>
                     <span className={styles.label}>Low </span>
-                    <span className={styles.value}> 1,234.00</span>
+                    <span className={styles.value}>{marketData?.Silver?.low}</span>
                   </div>
                   <div className={`${styles.bar_icon}`}>
                     <Image
@@ -150,14 +321,14 @@ const MainScreen = () => {
                   <div className={styles.bidAsk_wrap}>
                     <div className={styles.bid}>
                       <span className={styles.label}>BID</span>
-                      <span className={styles.bidValue}>1,234.00</span>
+                      <span className={styles.bidValue}>{marketData?.Silver?.bid}</span>
                     </div>
                   </div>
 
                   <div className={styles.bidAsk_wrap}>
                     <div className={styles.ask}>
                       <span className={styles.label}>ASK</span>
-                      <span className={styles.askValue}>1,234.00</span>
+                      <span className={styles.askValue}>{marketData?.Silver?.offer}</span>
                     </div>
                   </div>
                 </li>
@@ -210,8 +381,6 @@ const MainScreen = () => {
               {marqueeText}
             </span>
           ))}
-
-
         </div>
       </div>
       <div className={styles.background_lines}>
